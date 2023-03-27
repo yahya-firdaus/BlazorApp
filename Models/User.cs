39ad8ee593d7584bd647ddcf1d52e34da89838aa
root <root@DESKTOP-319LLRG.localdomain>
2023-03-27 11:43:04 +0700

    Upload

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
